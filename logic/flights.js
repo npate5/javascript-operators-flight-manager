5b9aab49af93ef9c16c611911835007f3deee1d3
neha <nehapatel8306@gmail.com>
2023-04-08 15:59:39 -0500

    new code

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,23 @@
 function Flights() {
+    function calculateNumberOfFlights(passengers, capacity) {
+        let flights;
 
+        if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {
+            throw new Error("The number of passengers must be a positive integer value")
+        }
+
+        if (passengers % capacity == 0) {
+            flights = passengers/capacity;
+        } else {
+            flights = Math.floor(passengers/capacity) + 1;
+        }
+
+        return flights;
+    }
+    return {calculateNumberOfFlights};
 }
+
+module.exports = Flights();
+
+
 
